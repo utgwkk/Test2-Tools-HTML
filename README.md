e7bb34182133995d407d87c6057f9cce0f5395b5
utgwkk <utagawakiki@gmail.com>
2020-10-27 11:28:50 +0000

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test2::Tools::HTML - It's new $module
+
+# SYNOPSIS
+
+    use Test2::Tools::HTML;
+
+# DESCRIPTION
+
+Test2::Tools::HTML is ...
+
+# LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+utgwkk <utagawakiki@gmail.com>
---
+++ lib/Test2/Tools/HTML.pm
@@ -0,0 +1,39 @@
+package Test2::Tools::HTML;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test2::Tools::HTML - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test2::Tools::HTML;
+
+=head1 DESCRIPTION
+
+Test2::Tools::HTML is ...
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
+
